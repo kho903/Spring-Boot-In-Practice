cd09f25034fe6818017dae30f9c70606938a9186
kho903 <gmldnr2222@naver.com>
2023-09-11 22:32:23 +0900

    feat: add app.timeout and log

--- ch02/os-env-variables/src/main/java/com/jikim/ch02/OsEnvVariablesApplication.java
+++ ch02/os-env-variables/src/main/java/com/jikim/ch02/OsEnvVariablesApplication.java
@@ -1,13 +1,23 @@
 package com.jikim.ch02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class OsEnvVariablesApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(OsEnvVariablesApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(OsEnvVariablesApplication.class, args);
+		ConfigurableApplicationContext applicationContext
+			= SpringApplication.run(OsEnvVariablesApplication.class, args);
+		Environment env = applicationContext.getBean(Environment.class);
+		log.info("Configuration application timeout value : " +
+			env.getProperty("app.timeout"));
 	}
 
 }
