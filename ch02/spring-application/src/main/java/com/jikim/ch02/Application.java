e9eaa332deffb6e126a682de484969e8bb80b604
kho903 <gmldnr2222@naver.com>
2023-09-11 17:08:11 +0900

    feat: add setDefaultProperties() in Application class

--- ch02/spring-application/src/main/java/com/jikim/ch02/Application.java
+++ ch02/spring-application/src/main/java/com/jikim/ch02/Application.java
@@ -1,5 +1,7 @@
 package com.jikim.ch02;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		Properties properties = new Properties();
+		properties.setProperty("spring.config.on-not-found", "ignore");
+		SpringApplication application
+			= new SpringApplication(Application.class);
+		application.setDefaultProperties(properties);
+		application.run(args);
 	}
 
 }
