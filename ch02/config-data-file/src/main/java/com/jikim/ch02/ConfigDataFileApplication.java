6988e19705c554fe3c5cf1190644d2885ce486d7
kho903 <gmldnr2222@naver.com>
2023-09-11 17:54:40 +0900

    conf: start config-data-file project for 환경 설정 파일

---
+++ ch02/config-data-file/src/main/java/com/jikim/ch02/ConfigDataFileApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDataFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigDataFileApplication.class, args);
+	}
+
+}
