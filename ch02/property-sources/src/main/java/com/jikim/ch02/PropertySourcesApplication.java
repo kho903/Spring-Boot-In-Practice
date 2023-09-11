287fcbfbc2ce2b27d88dce7c4d11a6c3132f9445
kho903 <gmldnr2222@naver.com>
2023-09-11 17:14:45 +0900

    conf: start propertySources project for @PropertySource

---
+++ ch02/property-sources/src/main/java/com/jikim/ch02/PropertySourcesApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertySourcesApplication.class, args);
+	}
+
+}
---
+++ ch02/property-sources/src/test/java/com/jikim/ch02/PropertySourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikim.ch02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
