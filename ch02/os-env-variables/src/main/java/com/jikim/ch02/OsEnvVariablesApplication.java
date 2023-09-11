db551948bf8b6c40a5102807bda302907a2b4de7
kho903 <gmldnr2222@naver.com>
2023-09-11 18:48:31 +0900

    conf: start os-env-variables project for 운영 체제 환경 변수

---
+++ ch02/os-env-variables/src/main/java/com/jikim/ch02/OsEnvVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsEnvVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsEnvVariablesApplication.class, args);
+	}
+
+}
