aaa938d64682033cb519dfeb6b3b4bccae8b9d3e
kho903 <gmldnr2222@naver.com>
2023-09-12 15:59:49 +0900

    feat: logging with log4j2

--- ch02/logging-with-log4j2/src/main/java/com/jikim/ch02/LoggingWithLog4j2Application.java
+++ ch02/logging-with-log4j2/src/main/java/com/jikim/ch02/LoggingWithLog4j2Application.java
@@ -1,13 +1,18 @@
 package com.jikim.ch02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LoggingWithLog4j2Application {
 
+	private static final Logger log = LoggerFactory.getLogger(LoggingWithLog4j2Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(LoggingWithLog4j2Application.class, args);
+		log.info("LoggingWithLog4j2Application started successfully with Log4j2 configuration");
 	}
 
 }
