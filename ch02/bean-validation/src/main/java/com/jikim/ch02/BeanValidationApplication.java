e270105ecd3035ea7c65f5cfd8b3d78b175c579d
kho903 <gmldnr2222@naver.com>
2023-09-12 16:51:35 +0900

    feat: Validation in CommandLineRunner

--- ch02/bean-validation/src/main/java/com/jikim/ch02/BeanValidationApplication.java
+++ ch02/bean-validation/src/main/java/com/jikim/ch02/BeanValidationApplication.java
@@ -1,13 +1,42 @@
 package com.jikim.ch02;
 
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jikim.ch02.model.Course;
+
 @SpringBootApplication
-public class BeanValidationApplication {
+public class BeanValidationApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(BeanValidationApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeanValidationApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Course course = new Course();
+		course.setId(1);
+		course.setRating(0);
+
+		Validator validator
+			= Validation.buildDefaultValidatorFactory().getValidator();
+
+		Set<ConstraintViolation<Course>> violations =
+			validator.validate(course);
+
+		violations.forEach(courseConstraintViolation ->
+			log.error("A constraint violation has occurred. Violation details: [{}].", courseConstraintViolation)
+		);
+	}
 }
