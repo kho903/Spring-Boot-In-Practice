4e5590441cecd86793e3df881560a28e49b5a66e
kho903 <gmldnr2222@naver.com>
2023-09-12 14:37:46 +0900

    conf: start spring-boot-default-logging project for logging customizing

---
+++ ch02/spring-boot-default-logging/src/main/java/com/jikim/ch02/SpringBootDefaultLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDefaultLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDefaultLoggingApplication.class, args);
+	}
+
+}
