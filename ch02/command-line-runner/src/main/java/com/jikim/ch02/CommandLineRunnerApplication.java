86aaf60013533a84a6e5008e7d6b89226ea87978
kho903 <gmldnr2222@naver.com>
2023-09-12 12:54:45 +0900

    feat: CommandLineRunner Bean

--- ch02/command-line-runner/src/main/java/com/jikim/ch02/CommandLineRunnerApplication.java
+++ ch02/command-line-runner/src/main/java/com/jikim/ch02/CommandLineRunnerApplication.java
@@ -5,9 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class CommandLineRunnerApplication implements CommandLineRunner {
+public class CommandLineRunnerApplication /*implements CommandLineRunner*/ {
 
 	protected static final Logger log = LoggerFactory.getLogger(CommandLineRunnerApplication.class);
 
@@ -15,8 +16,18 @@ public class CommandLineRunnerApplication implements CommandLineRunner {
 		SpringApplication.run(CommandLineRunnerApplication.class, args);
 	}
 
-	@Override
+	/*@Override
 	public void run(String... args) throws Exception {
 		log.info("CommandLineRunnerApplication CommandLineRunner has Executed");
+	}*/
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			log.info("CommandLineRunner executed as a bean definition with " + args.length + " arguments");
+			for (String arg : args) {
+				log.info("Argument: " + arg);
+			}
+		};
 	}
 }
