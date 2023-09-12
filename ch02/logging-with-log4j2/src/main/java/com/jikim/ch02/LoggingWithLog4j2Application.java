250dff3a8a4515076e126d612cb06470f8aaca82
kho903 <gmldnr2222@naver.com>
2023-09-12 15:26:47 +0900

    conf: start logging-with-log4j2 project for using log4j2

---
+++ ch02/logging-with-log4j2/src/main/java/com/jikim/ch02/LoggingWithLog4j2Application.java
@@ -0,0 +1,13 @@
+package com.jikim.ch02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingWithLog4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingWithLog4j2Application.class, args);
+	}
+
+}
