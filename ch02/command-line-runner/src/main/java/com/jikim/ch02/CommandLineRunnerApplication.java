9b84928da28b655d5e63f23d730f103a5fbb4363
kho903 <gmldnr2222@naver.com>
2023-09-12 12:46:52 +0900

    feat: implements CommandLineRunner run() method in main class

--- ch02/command-line-runner/src/main/java/com/jikim/ch02/CommandLineRunnerApplication.java
+++ ch02/command-line-runner/src/main/java/com/jikim/ch02/CommandLineRunnerApplication.java
@@ -1,13 +1,22 @@
 package com.jikim.ch02;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommandLineRunnerApplication {
+public class CommandLineRunnerApplication implements CommandLineRunner {
+
+	protected static final Logger log = LoggerFactory.getLogger(CommandLineRunnerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommandLineRunnerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("CommandLineRunnerApplication CommandLineRunner has Executed");
+	}
 }
