d8737204094d32f594b042279b4b39119925d1bc
kho903 <gmldnr2222@naver.com>
2023-09-18 18:37:29 +0900

    conf: start custom-healthindicator project

---
+++ ch04/custom-healthindicator/src/main/java/com/jikim/ch04/CustomHealthindicatorApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.ch04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomHealthindicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomHealthindicatorApplication.class, args);
+	}
+
+}
