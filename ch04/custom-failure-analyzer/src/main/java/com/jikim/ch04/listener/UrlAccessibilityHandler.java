b4a8c06ea5e493cbb113fdf97b0b68abdff916d9
kho903 <gmldnr2222@naver.com>
2023-09-18 14:10:23 +0900

    feat: UrlAccessibilityHandler
    - 외부 API URL에 접근을 시도하는 클래스

---
+++ ch04/custom-failure-analyzer/src/main/java/com/jikim/ch04/listener/UrlAccessibilityHandler.java
@@ -0,0 +1,21 @@
+package com.jikim.ch04.listener;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.jikim.ch04.exception.UrlNotAccessibleException;
+
+@Component
+public class UrlAccessibilityHandler {
+	@Value("${api.url:https://dog.ceo/}")
+	private String url;
+
+	@EventListener(classes = ContextRefreshedEvent.class)
+	public void listen() {
+		// 외부 API URL에 접근할 수 없는 상태를 만들기 위해
+		// 의도적으로 UrlNotAccessibleException 예외를 던지도록 작성
+		throw new UrlNotAccessibleException(url);
+	}
+}
