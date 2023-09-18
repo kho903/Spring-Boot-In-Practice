8eabac6fb9daf6bbbb410ef2714f4493b15041e8
kho903 <gmldnr2222@naver.com>
2023-09-18 19:03:42 +0900

    conf: start spring-boot-actuator-info-actuator project

---
+++ ch04/spring-boot-actuator-info-endpoint/src/main/java/com/jikim/ch04/SpringBootActuatorInfoEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.ch04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorInfoEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootActuatorInfoEndpointApplication.class, args);
+	}
+
+}
