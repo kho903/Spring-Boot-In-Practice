7962a03a8fe167556364cbba9a063aa65c38471e
kho903 <gmldnr2222@naver.com>
2023-09-18 14:01:31 +0900

    conf: start custom-failure-analyzer project

---
+++ ch04/custom-failure-analyzer/src/main/java/com/jikim/ch04/CustomFailureAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.jikim.ch04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomFailureAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomFailureAnalyzerApplication.class, args);
+	}
+
+}
---
+++ ch04/custom-failure-analyzer/src/test/java/com/jikim/ch04/CustomFailureAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jikim.ch04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomFailureAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
