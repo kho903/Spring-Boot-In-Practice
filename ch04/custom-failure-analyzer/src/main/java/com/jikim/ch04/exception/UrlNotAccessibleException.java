7853e9a03e4c2b3fa60e31e3aae6d0918b08dfa3
kho903 <gmldnr2222@naver.com>
2023-09-18 14:04:55 +0900

    feat: UrlNotAccessibleException
    - 애플리케이션이 의존하는 외부 API URL 접근에 실패하면 발생하는 런타임 예외

---
+++ ch04/custom-failure-analyzer/src/main/java/com/jikim/ch04/exception/UrlNotAccessibleException.java
@@ -0,0 +1,18 @@
+package com.jikim.ch04.exception;
+
+import lombok.Getter;
+
+@Getter
+public class UrlNotAccessibleException extends RuntimeException {
+
+	private String url;
+
+	public UrlNotAccessibleException(String url) {
+		this(url, null);
+	}
+
+	public UrlNotAccessibleException(String url, Throwable cause) {
+		super("URL " + url + " is not accessible", cause);
+		this.url = url;
+	}
+}
